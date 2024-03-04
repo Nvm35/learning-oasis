945fad1ef3784f5df649a785866e7ce9b6a6c732
Nvm35 <twikk3@gmail.com>
2024-03-04 23:36:17 +0500

    bookings

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,3 @@
-
-
 import { createClient } from '@supabase/supabase-js'
 export const supabaseUrl = 'https://uugggaqtfnyhmxufhelk.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1Z2dnYXF0Zm55aG14dWZoZWxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0MTAyODIsImV4cCI6MjAyMzk4NjI4Mn0.-Uf7QwxMAgPcm-ym5ieEhvrjntewu5gViWg7Lk8aZZE'
