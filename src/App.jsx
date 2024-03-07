9566dbd60be88e64956b13dda71d76f7dc489e94
Nvm35 <twikk3@gmail.com>
2024-03-07 15:32:29 +0500

    booking

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import Booking from "./pages/Booking";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,7 +32,8 @@ export default function App() {
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
-            <Route path="booking" element={<Bookings />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
@@ -17,21 +19,27 @@ export async function getBooking(id) {
   return data;
 }
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from('bookings')
-    .select("id, created_at, startDate, endDate, numNight, numGuests, status, totalPrice, cabins(name), guests(fullName, email)")
+    .select("id, created_at, startDate, endDate, numNight, numGuests, status, totalPrice, cabins(name), guests(fullName, email)", { count: 'exact' });
   if (filter) query = query[filter.method || "eq"](filter.field, filter.value)
 
   if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" })
 
-  const { data, error } = await query;
+  if (page) {
+    const from = (page - 1) * (PAGE_SIZE - 1)
+    const to = page + (PAGE_SIZE - 1)
+    query = query.range(from, to)
+  }
+
+  const { data, error, count } = await query;
 
   if (error) {
     console.log(error)
     throw new Error("Booking could not be loaded")
   }
-  return data;
+  return { data, count };
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
