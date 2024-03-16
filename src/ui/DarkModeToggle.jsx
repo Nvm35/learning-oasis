b203f5d596dae4e8ac5bee397d7b4ec4afea3ac5
Nvm35 <twikk3@gmail.com>
2024-03-16 16:14:08 +0500

    darkMode1

---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,10 @@
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineMoon } from "react-icons/hi2";
+
+export default function DarkModeToggle() {
+  return (
+    <ButtonIcon>
+      <HiOutlineMoon />
+    </ButtonIcon>
+  );
+}
